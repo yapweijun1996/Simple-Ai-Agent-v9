a56aaabdccd1f8a3502729350cda43b171b4c0c1
YAP WEI JUN <yapweijun1996@gmail.com>
2025-05-07 15:08:09 +0800

    zzz

---
+++ src/index.js
@@ -0,0 +1,8 @@
+// Entry point for Rollup bundling
+// Import modules to include them in the bundle in the right order
+import './utils.js';
+import './api-service.js';
+import './ui-controller.js';
+import './chat-controller.js';
+import './settings-controller.js';
+import './app.js'; 
